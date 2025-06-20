c81a35d823e603c857c3f764b82a0bc7e89a7481
Xiaowei TAN <39715117+Xiaowei-TAN@users.noreply.github.com>
2025-06-20 10:41:45 +0800

    Add files via upload

---
+++ CMakeFiles/gaitEventDetect.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sia/TIM_GED_Code/fsrReading.cpp" "/home/sia/TIM_GED_Code/CMakeFiles/gaitEventDetect.dir/fsrReading.cpp.o"
+  "/home/sia/TIM_GED_Code/logCreate.cpp" "/home/sia/TIM_GED_Code/CMakeFiles/gaitEventDetect.dir/logCreate.cpp.o"
+  "/home/sia/TIM_GED_Code/main.cpp" "/home/sia/TIM_GED_Code/CMakeFiles/gaitEventDetect.dir/main.cpp.o"
+  "/home/sia/TIM_GED_Code/signalInterrupt.cpp" "/home/sia/TIM_GED_Code/CMakeFiles/gaitEventDetect.dir/signalInterrupt.cpp.o"
+  "/home/sia/TIM_GED_Code/timeInterval.cpp" "/home/sia/TIM_GED_Code/CMakeFiles/gaitEventDetect.dir/timeInterval.cpp.o"
+  "/home/sia/TIM_GED_Code/xsensImu.cpp" "/home/sia/TIM_GED_Code/CMakeFiles/gaitEventDetect.dir/xsensImu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "./Xsens/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/gaitEventDetect.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gaitEventDetect.dir/main.cpp.o"
+  "CMakeFiles/gaitEventDetect.dir/signalInterrupt.cpp.o"
+  "CMakeFiles/gaitEventDetect.dir/timeInterval.cpp.o"
+  "CMakeFiles/gaitEventDetect.dir/logCreate.cpp.o"
+  "CMakeFiles/gaitEventDetect.dir/xsensImu.cpp.o"
+  "CMakeFiles/gaitEventDetect.dir/fsrReading.cpp.o"
+  "gaitEventDetect.pdb"
+  "gaitEventDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gaitEventDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
